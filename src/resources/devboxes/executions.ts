2b0609fe280051ef4ab38f37659a6dbcee6a8486
Tony Deng <tony@runloop.ai>
2025-09-09 17:27:56 -0700

    add default option for offset

--- src/resources/devboxes/executions.ts
+++ src/resources/devboxes/executions.ts
@@ -156,7 +156,7 @@ export class Executions extends APIResource {
     };
     const getStream: (offset: number | undefined) => APIPromise<Stream<ExecutionUpdateChunk>> = (offset) =>
       this._client.get(`/v1/devboxes/${devboxId}/executions/${executionId}/stream_stderr_updates`, {
-        query: { ...query, offset: offset?.toString() },
+        query: { ...query, offset: offset?.toString() || '0' },
         ...mergedOptions,
         stream: true,
       });
